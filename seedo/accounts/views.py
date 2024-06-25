f87ed89ae197689367a8f4616205f2ab4696f2d5
suzing <wltnwls66@gmail.com>
2024-06-25 11:44:54 +0900

    create common

--- seedo/accounts/views.py
+++ seedo/accounts/views.py
@@ -1,15 +1,15 @@
 # accounts/views.py
 
-from functools import wraps
 
 import jwt
 from django.conf import settings
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout as auth_logout
-from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.decorators.csrf import csrf_protect
+
+from SeedoPJT.seedo.common.views import token_required
 
 from .forms import CustomAuthenticationForm, CustomUserCreationForm
 from .models import RefreshToken
@@ -57,43 +57,6 @@ class LoginView(View):
                 return response
 
         return render(request, "login.html", {"form": form})
-
-
-def token_required(view_func):
-    @wraps(view_func)
-    def wrapper(request, *args, **kwargs):
-        access_token = request.COOKIES.get("access_token")
-        refresh_token = request.COOKIES.get("refresh_token")
-
-        if not access_token or not refresh_token:
-            return JsonResponse({"error": "Authentication required"}, status=401)
-
-        try:
-            decoded_token = jwt.decode(access_token, "seedo_project_key", algorithms=["HS256"])
-            request.user_id = decoded_token["user_id"]
-        except jwt.ExpiredSignatureError:
-            try:
-                decoded_refresh_token = jwt.decode(refresh_token, "seedo_project_key", algorithms=["HS256"])
-                user_id = decoded_refresh_token["user_id"]
-                refresh_token_instance = RefreshToken.objects.get(user_id=user_id, token=refresh_token)
-                if refresh_token_instance.token_blacklist:
-                    return JsonResponse({"error": "Refresh Token is blacklisted"}, status=401)
-
-                new_access_token = jwt.encode({"user_id": user_id}, "seedo_project_key", algorithm="HS256")
-                if isinstance(new_access_token, bytes):
-                    new_access_token = new_access_token.decode("utf-8")
-
-                response = JsonResponse({"access_token": new_access_token})
-                response.set_cookie("access_token", new_access_token)
-                return response
-            except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, RefreshToken.DoesNotExist):
-                return JsonResponse({"error": "Invalid or expired Refresh Token"}, status=401)
-        except jwt.InvalidTokenError:
-            return JsonResponse({"error": "Invalid Access Token"}, status=401)
-
-        return view_func(request, *args, **kwargs)
-
-    return wrapper
 
 
 @token_required
--- seedo/config/settings.py
+++ seedo/config/settings.py
@@ -96,7 +96,7 @@ JWT_EXPIRATION_DELTA = datetime.timedelta(hours=1)
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+DATABASES = {"default": {"ENGINE": "django.db.backends.mysql", "NAME": "django_db"}}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
