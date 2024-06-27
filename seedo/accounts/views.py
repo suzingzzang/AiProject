3bbb6b67c4a7bce32bebaca793b3902d1eb47046
jinho4124 <jh.yoon@sparta-devcamp.com>
2024-06-27 13:28:01 +0900

    fix: 깃토큰검증수정

--- seedo/accounts/views.py
+++ seedo/accounts/views.py
@@ -2,7 +2,7 @@
 
 
 import jwt
-from django.conf import settings
+from common.utils import generate_tokens
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
@@ -12,7 +12,6 @@ from django.views.decorators.csrf import csrf_protect
 
 from .forms import CustomAuthenticationForm, CustomUserCreationForm
 from .models import RefreshToken
-from .utils import generate_tokens
 
 
 class SignUpView(View):
@@ -36,6 +35,7 @@ class LoginView(View):
     def post(self, request):
         form = CustomAuthenticationForm(data=request.POST)
         if form.is_valid():
+            logout(request)
             email = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
             user = authenticate(request, email=email, password=password)
@@ -43,19 +43,13 @@ class LoginView(View):
                 # 토큰 생성
                 access_token, refresh_token = generate_tokens(user)
 
-                # 리프레시 토큰 저장 또는 갱신
-                refresh_token_instance, created = RefreshToken.objects.get_or_create(user=user)
-                refresh_token_instance.token = refresh_token
-                refresh_token_instance.token_blacklist = False
-                refresh_token_instance.save()
-
                 # 세션을 사용하여 request.user 설정
                 auth_login(request, user)
 
                 # 클라이언트 측에 토큰 저장
                 response = redirect("home")
-                response.set_cookie("access_token", access_token, max_age=settings.JWT_ACCESS_TOKEN_EXPIRATION)
-                response.set_cookie("refresh_token", refresh_token, max_age=settings.JWT_REFRESH_TOKEN_EXPIRATION)
+                response.set_cookie("access_token", access_token)
+                response.set_cookie("refresh_token", refresh_token)
                 return response
 
         return render(request, "accounts/login.html", {"form": form})
@@ -69,14 +63,15 @@ def logout(request):
             decoded_token = jwt.decode(refresh_token, "seedo_project_key", algorithms=["HS256"])
             refresh_token_instance = RefreshToken.objects.get(user_id=decoded_token["user_id"], token=refresh_token)
             refresh_token_instance.token_blacklist = True
-            print(refresh_token_instance.token_blacklist, "refresh")
             refresh_token_instance.save()
         except (jwt.InvalidTokenError, RefreshToken.DoesNotExist):
             pass
 
     auth_logout(request)
-    response = render(request, "accounts/login.html")
-    response.delete_cookie("access_token")
-    response.delete_cookie("refresh_token")
 
-    return response
+    if request.headers["Referer"][30:] != "/login/":
+        response = redirect("accounts:login")
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+
+        return response
--- seedo/common/decorators.py
+++ seedo/common/decorators.py
@@ -1,9 +1,8 @@
 from functools import wraps
 
-import jwt
 from accounts.models import RefreshToken
-from django.conf import settings
-from django.http import HttpResponseRedirect, JsonResponse
+from common.utils import generate_new_access_token, verify_access_token, verify_refresh_token
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
@@ -16,28 +15,29 @@ def token_required(view_func):
         if not access_token or not refresh_token:
             return HttpResponseRedirect(reverse("accounts:login"))
 
-        try:
-            decoded_token = jwt.decode(access_token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+        decoded_token = verify_access_token(access_token)
+        if decoded_token:
             request.user_id = decoded_token["user_id"]
-        except jwt.ExpiredSignatureError:
-            try:
-                decoded_refresh_token = jwt.decode(refresh_token, settings.JWT_REFRESH_SECRET_KEY, algorithms=["HS256"])
+        else:
+            decoded_refresh_token = verify_refresh_token(refresh_token)
+            if decoded_refresh_token:
                 user_id = decoded_refresh_token["user_id"]
-                refresh_token_instance = RefreshToken.objects.get(user_id=user_id, token=refresh_token)
-                if refresh_token_instance.token_blacklist:
+                try:
+                    refresh_token_instance = RefreshToken.objects.get(user_id=user_id, token=refresh_token)
+                    if refresh_token_instance.token_blacklist:
+                        return HttpResponseRedirect(reverse("accounts:login"))
+
+                    new_access_token = generate_new_access_token(user_id)
+                    if isinstance(new_access_token, bytes):
+                        new_access_token = new_access_token.decode("utf-8")
+
+                    response = view_func(request, *args, **kwargs)
+                    response.set_cookie("access_token", new_access_token)
+                    return response
+                except RefreshToken.DoesNotExist:
                     return HttpResponseRedirect(reverse("accounts:login"))
-
-                new_access_token = jwt.encode({"user_id": user_id}, settings.JWT_SECRET_KEY, algorithm="HS256")
-                if isinstance(new_access_token, bytes):
-                    new_access_token = new_access_token.decode("utf-8")
-
-                response = JsonResponse({"access_token": new_access_token})
-                response.set_cookie("access_token", new_access_token)
-                return response
-            except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, RefreshToken.DoesNotExist):
+            else:
                 return HttpResponseRedirect(reverse("accounts:login"))
-        except jwt.InvalidTokenError:
-            return HttpResponseRedirect(reverse("accounts:login"))
 
         return view_func(request, *args, **kwargs)
 
