ea7e69ae80062b4a240a8c864e458ca7529d88ad
palkapa <kangspa98@naver.com>
2024-07-09 11:37:37 +0900

    split:navi_js_css

--- seedo/config/settings.py
+++ seedo/config/settings.py
@@ -154,6 +154,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "accounts", "static"),
     os.path.join(BASE_DIR, "record", "static"),
+    os.path.join(BASE_DIR, "qna", "static"),
+    os.path.join(BASE_DIR, "navigation", "static"),
 ]  # 프로젝트 수준의 static 디렉토리
 
 
